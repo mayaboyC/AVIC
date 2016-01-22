809cf298de906ff82d8a9617473bceb3573fd048
mayaboyc <zhaoliang@cannews.com.cn>
2016-01-22 13:11:13 +0800

    updata

--- static/js/tomenu.js
+++ static/js/tomenu.js
@@ -1,6 +1,6 @@
 function tomenu(){
     var menu = document.getElementById("menu");
-    if(menu.style.display == "none"){
+    if(menu.style.display == "none" || menu.style.display == ""){
         menu.style.display ="block";
     }else{
         menu.style.display = "none";
